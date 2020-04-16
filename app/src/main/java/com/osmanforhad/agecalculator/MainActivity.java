0d1690abd2bba9d84c0d702518510225655ee67a
osman forhad <osmanforhad@pop-os.localdomain>
2020-04-16 22:53:50 +0600

    fresh application program

---
+++ app/src/main/java/com/osmanforhad/agecalculator/MainActivity.java
@@ -0,0 +1,15 @@
+package com.osmanforhad.agecalculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }//end of the oncreate method
+
+}//end of the class
